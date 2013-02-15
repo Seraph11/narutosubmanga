6ecef0f3620b708c5a9e532be901052391510a0d
Mirwing <mirwingr@gmail.com>
2013-02-15 12:08:22 -0300

    Fixes #1

---
+++ pdf_converter.rb
@@ -0,0 +1,22 @@
+# encoding: UTF-8
+
+require 'singleton'
+class PdfConverter
+  
+  include Singleton
+
+  def converter(source_path, file_name='my_document')
+    convert = %x[which convert].strip
+    return nil unless File.exists? convert
+
+    source_path = source_path.strip.chomp('/')
+    return nil unless Dir.exists? source_path
+    %x[convert #{sanitize(source_path)}/*.??g #{sanitize(source_path)}/#{sanitize(file_name)}.pdf]
+    return $?.exitstatus == 0 ? true : false
+  end
+
+  def sanitize(str)
+    Regexp.escape(str).gsub(/'/,"\\\\'")
+  end
+  
+end
